c7b5ed447a4ebda3285fc765d3735bbe17117a41
Fire <pascaldebruijngsm@gmail.com>
2021-01-18 16:38:18 +0100

    dice roller start

---
+++ Server application/Dice Roller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class DiceRoller
+{
+	public int Dice()
+    {
+		return Dice
+    }
+	
+	
+	public DiceRoller()
+	{
+		Random dice = dice
+		int dice = dice.Next(dice)
+	}
+}
